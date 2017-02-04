32a443b51e58a874f1e9d774d6e96a0f987b0df4
zhaofuqiang <1586687169@qq.com>
2017-02-04 17:29:31 +0800

    Initial Commit

---
+++ LogDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LogDemo
+//
+//  Created by _ on 17/2/4.
+//  Copyright © 2017年 zhaofuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LogDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogDemo
+//
+//  Created by _ on 17/2/4.
+//  Copyright © 2017年 zhaofuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
