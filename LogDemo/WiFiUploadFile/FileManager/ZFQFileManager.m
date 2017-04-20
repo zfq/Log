e255e2113e77590103a68a8dcff45e00ade92383
zhaofuqiang <1586687169@qq.com>
2017-04-20 11:24:52 +0800

    支持多线程

--- LogDemo/WiFiUploadFile/FileManager/ZFQFileManager.m
+++ LogDemo/WiFiUploadFile/FileManager/ZFQFileManager.m
@@ -12,45 +12,39 @@
 #import "ZFQDBPromise.h"
 
 @interface ZFQFileManager()
-@property (nonatomic, strong) FMDatabase *database;
-@property (nonatomic, strong) FMDatabaseQueue *queue;
+
 @end
 
 @implementation ZFQFileManager
 
-- (instancetype)init
++ (FMDatabaseQueue *)sharedDBQueue
 {
-    self = [super init];
-    if (self) {
-        [self createTableIfNotExist];
-    }
-    return self;
+    /**
+     Since there are may be several ZFQFileManager instance in multithreading, 
+     so these instance should be use a shared database queue.
+     */
+    static FMDatabaseQueue *sharedQueue = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        //First create a database if needed.
+        NSString *dbPath = [[self class] dbPath];
+        [FMDatabase databaseWithPath:dbPath];
+        //then create a shared serial queue.
+        sharedQueue = [FMDatabaseQueue databaseQueueWithPath:dbPath];
+        //finally create table.
+        [[self class] createTableIfNotExistOnQueue:sharedQueue];
+    });
+    return sharedQueue;
 }
 
 #pragma mark - Public
-- (void)createTableIfNotExist
-{
-    FMDatabaseQueue *queue = [FMDatabaseQueue databaseQueueWithPath:[self dbPath]];
-    
-    //create table
-    [queue inDatabase:^(FMDatabase *db) {
-        NSString *sql = @"CREATE TABLE IF NOT EXISTS wifi_files ( \
-        file_id INTEGER PRIMARY KEY, \
-        path TEXT, \
-        name TEXT \
-        );";
-        if (![db executeUpdate:sql]) {
-            ZFQLog(@">>create table wifi_files failed ,%@", [db lastError]);
-        }
-    }];
-    
-}
 
 - (ZFQDBPromise *)executeUpdate:(NSString *)sql
 {
     ZFQDBPromise *promise = [ZFQDBPromise promiseWithAdapterBlock:^(ZFQDBPromiseAdapter *adapter) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [self.queue inDatabase:^(FMDatabase *db) {
+            FMDatabaseQueue *queue = [[self class] sharedDBQueue];
+            [queue inDatabase:^(FMDatabase *db) {
                 if (![db executeUpdate:sql]) {
                     dispatch_async(dispatch_get_main_queue(), ^{
                         adapter.reject([db lastError]);
@@ -73,15 +67,20 @@
 {
     ZFQDBPromise *promise = [ZFQDBPromise promiseWithAdapterBlock:^(ZFQDBPromiseAdapter *adapter) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            [self.queue inDatabase:^(FMDatabase *db) {
+            FMDatabaseQueue *queue = [[self class] sharedDBQueue];
+            [queue inDatabase:^(FMDatabase *db) {
                 NSError *error;
-                id result = [db executeQuery:sql values:values error:&error];
+                FMResultSet *result = [db executeQuery:sql values:values error:&error];
                 if (error) {
                     dispatch_async(dispatch_get_main_queue(), ^{
                         adapter.reject(error);
+                        [result close];
                     });
                 } else {
-                    dispatch_async(dispatch_get_main_queue(), ^{
+//                    dispatch_async(dispatch_get_main_queue(), ^{
+//                        adapter.resolve(result);
+//                    });
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                         adapter.resolve(result);
                     });
                 }
@@ -120,47 +119,31 @@
 
 - (ZFQDBPromise *)searchFileWithFileId:(NSInteger)fileId
 {
-//    return [self executeQuery:@"SELECT name,path FROM wifi_files WHERE file_id=?" values:@[@(fileId)]];
-    
-    ZFQDBPromise *promise = [ZFQDBPromise promiseWithAdapterBlock:^(ZFQDBPromiseAdapter *adapter) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            adapter.resolve(nil);
-        });
-        
-    }];
-    
-    return promise;
+    return [self executeQuery:@"SELECT name,path FROM wifi_files WHERE file_id=?" values:@[@(fileId)]];
 }
 
-#pragma mark - Getter Setter
-- (FMDatabase *)database
-{
-    if (!_database) {
-        _database = [self createDadabase];
-    }
-    return _database;
-}
-
-- (FMDatabaseQueue *)queue
-{
-    if (!_queue) {
-        _queue = [FMDatabaseQueue databaseQueueWithPath:[self dbPath]];
-    }
-    return _queue;
-}
 
 #pragma mark - Private
-- (NSString *)dbPath
++ (NSString *)dbPath
 {
     NSString *docsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
     NSString *dbPath   = [docsPath stringByAppendingPathComponent:@"wifi_files.db"];
     return dbPath;
 }
 
-- (FMDatabase *)createDadabase
++ (void)createTableIfNotExistOnQueue:(FMDatabaseQueue *)queue
 {
-    FMDatabase *db     = [FMDatabase databaseWithPath:[self dbPath]];
-    return db;
+    //create table
+    [queue inDatabase:^(FMDatabase *db) {
+        NSString *sql = @"CREATE TABLE IF NOT EXISTS wifi_files ( \
+        file_id INTEGER PRIMARY KEY, \
+        path TEXT, \
+        name TEXT \
+        );";
+        if (![db executeUpdate:sql]) {
+            ZFQLog(@">>create table wifi_files failed ,%@", [db lastError]);
+        }
+    }];
 }
 
 @end
